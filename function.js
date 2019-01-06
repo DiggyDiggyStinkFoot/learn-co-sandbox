4c36a9e684a4e63e7d64e4da4f5e0317694690a3
Wannawuth Damrongchai <wdamrongchai@hotmail.com>
2019-01-06 17:32:58 +0000

    Automatically backed up by Learn

---
+++ function.js
@@ -0,0 +1,64 @@
+function sayHello(){
+  console.log('Hello!')
+}
+sayHello()
+function sayHelloToHunter(){
+  console.log('Hello Hunter')
+}
+sayHelloToHunter()
+sayHelloToHunter()
+
+function tellWeather(){
+  console.log('weather outside is freightful')
+}
+tellWeather()
+
+function doSomething(thing){
+  console.log(thing)
+}
+
+doSomething('anything')
+
+function sayHelloTo(firstName){
+  console.log(`Hello,${firstName}!`)
+}
+
+sayHelloTo('Bob')
+sayHelloTo('Hunter')
+
+function say(greeting, firstName){
+  console.log (`${greeting},${firstName}!`)
+}
+
+say("Yo","Tyrone")
+
+function add(x,y){
+  return console.log(x+y)
+}
+add(1,2)
+
+
+
+function say(greeting, firstName){
+  return console.log(`${greeting}, ${firstName}!`)
+}
+console.log
+say("Hola","Juan")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
